632441c45fd605693809faca49a645a5f4fa14e0
DineishVS <dineishrocky@gmail.com>
2024-07-09 07:53:05 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,267 @@
+export const Bio = {
+  name: "Dineish VS",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "UI/UX Designer",
+    "Programmer",
+    "Data Analyst"
+  ],
+  description:
+    "As a B.Tech Computer Science and Business System student, I combine technical prowess with a strategic mindset.",
+  github: "https://github.com/DineishVS",
+  resume:
+    "https://drive.google.com/file/d/1XNvr4G5Dz2LgbPt84n98nI6u1xPPsU0k/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/dineishvs/",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Django",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    /*img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",*/
+    role: "Fullstack Engineering Intern",
+    company: "Vdart Inc,Trichy|Onsite",
+    date: "June 2024 - July 2024",
+    desc: "Resume Screening and Parsing website,Implemented feature to extract key information from uploaded resumes using a trained model,presenting the necessary data in a user friendly format and Added functionality to evaluate resumes based on job descriptions,Providing accuracy scores to help users optimize their resumes.",
+    skills: [
+      "Django",
+      "React",
+      "JavaScript",
+      "Python",
+    ],
+   
+  },
+  {
+    id: 1,
+    /*img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",*/
+    role: "Android Developer and UI/UX Designing Intern",
+    company: "eQuadringa Software Pvt Ltd,Trichy|Onsite",
+    date: "June 2023 - July 2023",
+    desc: "Designed a user-friendly smart bin application in Figma for a waste management initiative by the Trichy Governtment Corporationg and learned fundamental principles of Android development",
+    skills: [
+      "Flutter(Basics)",
+      "Kotlin(Basics)",
+      "Figma",
+      "Android Studio"
+    ],
+  },
+  {
+    id: 2,
+    /*img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",*/
+    role: "Event Organizer(Promotions) ",
+    company: "Entrepreneurship Club,Sastra",
+    date: "2021 - 2023",
+    desc: "Achieved significant results by attracting 150 attendees to events through target promotion efforts in a short timeframe",
+    /*doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",*/
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    /*img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",*/
+    school: "SASTRA DEEMED UNIVERSITY , Thanjavur",
+    date: "2021 -2025",
+    grade: "8.43 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Bussiness systems at Sastra Deemed University, Thanjavur. I have completed 6 semesters and have a CGPA of 8.43. ",
+    degree: "Bachelor of Technology - BTech, Computer Science and Business systems",
+  },
+  {
+    id: 1,
+    /*img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",*/
+    school: "Sri Rasi Vinayaga, Trichy",
+    date: "2020-2021",
+    grade: "92%",
+    desc: "I completed my class 12 high school education at SRV School, Trichy",
+    degree: "HSC(XII)",
+  },
+  {
+    id: 2,
+    /*img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",*/
+    school: "St.Annes mat hr sec school",
+    date: "2019",
+    grade: "90%",
+    desc: "I completed my class 10 education at St Annes School, Kumbakonam",
+    degree: "SSLC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Resume Screener and Parser",
+    date: "Jun 2024 - Jul 2024",
+    description:
+      "Developed a Userfriendly web application that Streamlines the resume screening and parsing process  ",
+    /*image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",*/
+    tags: [
+      "Python",
+      "Django",
+      "HTML",
+      "CSS",
+      "Javascript",
+    ],
+    category: "web app",
+    /*github: "https://github.com/rishavchanda/Trackify",*/
+    /*webapp: "https://trackify.duckdns.org",*/
+  },
+  {
+    id: 1,
+    title: "HR Recruiting CRM Application",
+    date: "Jun 2024 - Jul 2024",
+    description:
+      "Implemented features to facilitate seamless management and interaction with a candidate and job data  ",
+    /*image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",*/
+    tags: [
+      "Python",
+      "Django",
+      "HTML",
+      "CSS",
+      "Javascript",
+    ],
+    category: "web app",
+    
+    
+  },
+  {
+    id: 2,
+    title: "Expense Tracker",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a user-friendly web application using React to help College Students manage expenses effectively",
+    /*image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",*/
+    tags: ["React", "JavaScript", "HTML", "CSS"],
+    category: "web app",
+   
+  },
+  {
+    id: 3,
+    title: "IPL Score Prediction",
+    date: "Oct 2022 - Nov 2022",
+    description:
+      "Designed and Implemented a ML project to accurately forecast scores in IPL matches based on a historical data",
+    /*image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",*/
+    tags: [
+      "Linear Regression",
+      "Decision Trees",
+      "Random Forests",
+    ],
+    category: "web app",
+    /*github: "https://github",*/
+    
+  },
+];
+
+/*export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },*/
+
